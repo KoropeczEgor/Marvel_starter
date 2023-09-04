a5057b2274cf0705934a77d5cf2f40a6f4f2bc8a
Egor <koropeczegor@gmail.com>
2023-09-04 20:51:06 +0300

    project creation

---
+++ src/services/MarvelService.js
@@ -0,0 +1,25 @@
+class MarvelService {
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=ce4e3d41f5da6e896b528539f47b3746";
+  getResource = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  getAllCharacters = () => {
+    return this.getResource(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+    );
+  };
+
+  getCharacters = (id) => {
+    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+  };
+}
+
+export default MarvelService;
