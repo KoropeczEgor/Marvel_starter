b522da21532d65eb09d86cf32fa41f74a32b5f2e
Egor <koropeczegor@gmail.com>
2023-09-04 21:50:01 +0300

    data optimization

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -18,8 +18,25 @@ class MarvelService {
     );
   };
 
-  getCharacters = (id) => {
-    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+  getCharacters = async (id) => {
+    const res = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
+
+    return this._transformCharacter(res);
+  };
+
+  _transformCharacter = (res) => {
+    return {
+      name: res.data.results[0].name,
+      description: res.data.results[0].description,
+      thumbnail:
+        res.data.results[0].thumbnail.path +
+        "." +
+        res.data.results[0].thumbnail.extension,
+      homepage: res.data.results[0].urls[0].url,
+      wiki: res.data.results[0].urls[1].url,
+    };
   };
 }
 
