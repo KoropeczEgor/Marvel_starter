980861a9891f1906a560702c35d0f4b69152e912
Egor <koropeczegor@gmail.com>
2023-09-06 22:11:52 +0300

    image style change implementation randomchar__img

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -13,6 +13,8 @@ class RandomChar extends Component {
     error: false,
   };
 
+  marvelService = new MarvelService();
+
   componentDidMount() {
     this.updateChar();
     this.timerId = setInterval(this.updateChar, 10000);
@@ -22,10 +24,14 @@ class RandomChar extends Component {
     clearInterval(this.timerId);
   }
 
-  marvelService = new MarvelService();
-
   onCharLoaded = (char) => {
     this.setState({ char, loading: false });
+  };
+
+  onCharLoading = () => {
+    this.setState({
+      loading: true,
+    });
   };
 
   onError = () => {
@@ -35,6 +41,7 @@ class RandomChar extends Component {
   updateChar = () => {
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
 
+    this.onCharLoading();
     this.marvelService
       .getCharacters(id)
       .then(this.onCharLoaded)
@@ -71,10 +78,22 @@ class RandomChar extends Component {
 
 const View = ({ char }) => {
   const { name, description, thumbnail, homepage, wiki } = char;
+  let imgStyle = { objectFit: "cover" };
+  if (
+    thumbnail ===
+    "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+  ) {
+    imgStyle = { objectFit: "contain" };
+  }
 
   return (
     <div className="randomchar__block">
-      <img src={thumbnail} alt="Random character" className="randomchar__img" />
+      <img
+        src={thumbnail}
+        alt="Random character"
+        className="randomchar__img"
+        style={imgStyle}
+      />
       <div className="randomchar__info">
         <p className="randomchar__name">{name}</p>
         <p className="randomchar__descr">{description}</p>
