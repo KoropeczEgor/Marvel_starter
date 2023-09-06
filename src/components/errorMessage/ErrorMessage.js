88f8add6dcb8e554d39ef15f5764105e23d082d5
Egor <koropeczegor@gmail.com>
2023-09-06 22:03:02 +0300

    component changes ErrorMessage

--- src/components/errorMessage/ErrorMessage.js
+++ src/components/errorMessage/ErrorMessage.js
@@ -1,8 +1,19 @@
 import img from "./error.gif";
 
 const ErrorMessage = () => {
-  const style = `display: 'block', width: '250px', height: '250px', objectFit: 'contain', margin: '0 auto'`;
-  return <img style={style} src={img} alt="Error" />;
+  return (
+    <img
+      style={{
+        display: "block",
+        width: "250px",
+        height: "250px",
+        objectFit: "contain",
+        margin: "0 auto",
+      }}
+      src={img}
+      alt="Error"
+    />
+  );
 };
 
 export default ErrorMessage;
