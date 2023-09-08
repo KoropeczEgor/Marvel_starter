64aeb544ecb84945ec9b72be4c2aeab3a244acbb
Egor <koropeczegor@gmail.com>
2023-09-08 13:27:01 +0300

    fixes bug

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -11,6 +11,7 @@ class CharList extends Component {
     error: false,
     newItemLoading: false,
     offset: 210,
+    charEnded: false,
   };
 
   marvelService = new MarvelService();
@@ -21,7 +22,8 @@ class CharList extends Component {
 
   onRequest = (offset) => {
     this.onCharListLoading();
-    this.MarvelService.getAllCharacters(offset)
+    this.marvelService
+      .getAllCharacters(offset)
       .then(this.onCharListLoaded)
       .catch(this.onError);
   };
@@ -33,11 +35,17 @@ class CharList extends Component {
   };
 
   onCharListLoaded = (newCharList) => {
-    this.setState((offset, charList) => ({
+    let ended = false;
+    if (newCharList.length < 9) {
+      ended = true;
+    }
+
+    this.setState(({ offset, charList }) => ({
       charList: [...charList, ...newCharList],
       loading: false,
       newItemLoading: false,
       offset: offset + 9,
+      charEnded: ended,
     }));
   };
 
@@ -76,7 +84,8 @@ class CharList extends Component {
   }
 
   render() {
-    const { charList, loading, error, offset, newItemLoading } = this.state;
+    const { charList, loading, error, offset, newItemLoading, charEnded } =
+      this.state;
 
     const items = this.renderItems(charList);
 
@@ -92,6 +101,7 @@ class CharList extends Component {
         <button
           className="button button__main button__long"
           disabled={newItemLoading}
+          style={{ display: charEnded ? "none" : "block" }}
           onClick={() => this.onRequest(offset)}
         >
           <div className="inner">load more</div>
