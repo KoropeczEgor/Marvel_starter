10d4db44589c5af300114975249cfda0b236a1ef
Egor <koropeczegor@gmail.com>
2023-09-09 16:37:32 +0300

    media adaptation

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -17,7 +17,7 @@ class RandomChar extends Component {
 
   componentDidMount() {
     this.updateChar();
-    this.timerId = setInterval(this.updateChar, 10000);
+    this.timerId = setInterval(this.updateChar, 250000);
   }
 
   componentWillUnmount() {
