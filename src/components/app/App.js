21dafedcaba81fef3e7ec476af091f643c7a86de
Egor <koropeczegor@gmail.com>
2023-09-18 20:09:32 +0300

    separation of components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,51 +1,16 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ComicsList from "../comicsList/ComicsList";
-import AppBanner from "../appBanner/AppBanner";
-import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
-
-import decoration from "../../resources/img/vision.png";
 
 const App = () => {
-  const [selectedChar, setChar] = useState(null);
-
-  const onCharSelected = (id) => {
-    setChar(id);
-  };
-
   return (
-    // позволяет делать рабочими все ссылки в объекте Router
     <Router>
       <div className="app">
         <AppHeader />
         <main>
           <Switch>
-            {/* это маршрут Route */}
-            <Route exact path="/">
-              <ErrorBoundary>
-                <RandomChar />
-              </ErrorBoundary>
-
-              <div className="char__content">
-                <ErrorBoundary>
-                  <CharList onCharSelected={onCharSelected} />
-                </ErrorBoundary>
-
-                <ErrorBoundary>
-                  <CharInfo charId={selectedChar} />
-                </ErrorBoundary>
-              </div>
-              <img className="bg-decoration" src={decoration} alt="vision" />
-            </Route>
-            <Route exact path="/comics">
-              <AppBanner />
-              <ComicsList />
-            </Route>
+            <Route exact path="/"></Route>
+            <Route exact path="/comics"></Route>
           </Switch>
         </main>
       </div>
