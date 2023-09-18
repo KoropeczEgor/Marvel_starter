42f9ffa9a63a4923b3eb263d4324204f4ebe7280
Egor <koropeczegor@gmail.com>
2023-09-18 19:41:38 +0300

    work with react-router-dom

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
@@ -18,27 +19,37 @@ const App = () => {
   };
 
   return (
-    <div className="app">
-      <AppHeader />
-      <main>
-        {/* <ErrorBoundary>
-          <RandomChar />
-        </ErrorBoundary>
+    // позволяет делать рабочими все ссылки в объекте Router
+    <Router>
+      <div className="app">
+        <AppHeader />
+        <main>
+          <Switch>
+            {/* это маршрут Route */}
+            <Route exact path="/">
+              <ErrorBoundary>
+                <RandomChar />
+              </ErrorBoundary>
 
-        <div className="char__content">
-          <ErrorBoundary>
-            <CharList onCharSelected={onCharSelected} />
-          </ErrorBoundary>
+              <div className="char__content">
+                <ErrorBoundary>
+                  <CharList onCharSelected={onCharSelected} />
+                </ErrorBoundary>
 
-          <ErrorBoundary>
-            <CharInfo charId={selectedChar} />
-          </ErrorBoundary>
-        </div>
-        <img className="bg-decoration" src={decoration} alt="vision" /> */}
-        <AppBanner />
-        <ComicsList />
-      </main>
-    </div>
+                <ErrorBoundary>
+                  <CharInfo charId={selectedChar} />
+                </ErrorBoundary>
+              </div>
+              <img className="bg-decoration" src={decoration} alt="vision" />
+            </Route>
+            <Route exact path="/comics">
+              <AppBanner />
+              <ComicsList />
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 };
 
