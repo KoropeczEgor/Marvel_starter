c985548cbcfb3ea31ae2ddce0a91aca16e9ad35c
Egor <koropeczegor@gmail.com>
2023-09-18 14:19:23 +0300

    Comics listing page created

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import App from "./components/app/App";
 
 import "./style/style.scss";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
